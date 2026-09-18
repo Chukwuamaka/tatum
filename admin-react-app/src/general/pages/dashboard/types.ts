e04aab2dfed133895cdc5a3d234c08952a7a1a11
Chukwuamaka <osujichukwuamaka@gmail.com>
2026-09-18 08:26:18 +0100

    Delete redundant files and code

--- admin-react-app/src/general/pages/dashboard/types.ts
+++
@@ -1,6 +0,0 @@
-export interface SearchQueryState {
-  query: string;
-  updateQuery: (value: string) => void;
-  page: number;
-  setPage: React.Dispatch<React.SetStateAction<number>>;
-}
--- admin-react-app/src/general/reusables/Skeleton.tsx
+++
@@ -1,9 +0,0 @@
-interface SkeletonProps {
-  className?: string;
-}
-
-function Skeleton({ className = "" }: SkeletonProps) {
-  return <div className={`animate-pulse rounded bg-[#e5e7eb] ${className}`} />;
-}
-
-export default Skeleton;
